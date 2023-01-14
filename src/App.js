5bee22872b3ce3150174112f6927e68a0697fae5
NavxsVS <118521280+iadhzefzef1@users.noreply.github.com>
2023-01-14 09:09:07 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./Components/Form";
+import List from "./Components/List";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/List.js
@@ -0,0 +1,26 @@
+import "./list.css";
+import { useSelector, useDispatch } from "react-redux";
+const List = () => {
+  const tasks = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+  const deleteHandler = (event) => {
+    dispatch({ type: "delete-task", id: event.target.value });
+  };
+
+  return (
+    <ul>
+      {tasks.map((task, id) => (
+        <li key={id}>
+          {" "}
+          <p>{task}</p>{" "}
+          <button value={id} onClick={deleteHandler}>
+            X
+          </button>{" "}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
